d46274c26b003956a5a5f31d01f7bd6fcfa32311
Kaldari <rkaldari@wikimedia.org>
2017-10-18 15:46:59 -0700

    initial code and README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # jquery.badge
-jQuery plugin to add a counter badge to a DOM element
+
+This is a simple jQuery plug-in to overlay a counter badge on top of another element on a page.
+
+To use it, simply chain the function onto a jQuery DOM element. For example, if you have the following HTML ...
+```html
+<div id="inbox" class="button">My Inbox</div>
+```
+
+... and you apply the following JavaScript ...
+```javascript
+$( '#inbox' ).badge( 8 );
+```
+
+... you will get ...
+![button with badge](example.png)
+
+You can pass either a number or a text string as the first parameter. If you pass 0, false, or an empty string, the badge will not be displayed (although see "Advanced options" below).
+
+Note: Make sure the parent element has its position set to `relative`, or the badge may not appear in the correct location.
+
+===Advanced options
+You can control the location of the badge by passing a second parameter to the badge function. The parameter must be set to "top", "bottom", or "inline".
+```
+$( '#inbox' ).badge( 8, 'bottom' );
+
+You can force the badge to display zeroes by passing `true` as a third parameter.
+```
+$( '#inbox' ).badge( 0, 'top', true );
---
+++ jquery.badge.js
@@ -0,0 +1,86 @@
+/*
+ * jQuery Badge plugin
+ *
+ * @license MIT
+ *
+ * @author Ryan Kaldari <rkaldari@wikimedia.org>, 2012
+ * @author Andrew Garrett <agarrett@wikimedia.org>, 2012
+ * @author Marius Hoch <hoo@online.de>, 2012
+ */
+
+/**
+ * @class jQuery.plugin.badge
+ */
+( function ( $ ) {
+	/**
+	 * Put a badge on an item on the page. The badge container will be appended to the selected element(s).
+	 *
+	 *     $element.badge( 5 );
+	 *     $element.badge( '100+' );
+	 *     $element.badge( 'New', 'inline' );
+	 *     $element.badge( 0, 'upper-right', true );
+	 *
+	 * @param {number|string} text The value to display in the badge. If the value is falsey (0,
+	 *  null, false, '', etc.), any existing badge will be removed.
+	 * @param {string} [position=top] The position of the badge. Options are:
+	 *  inline, top, bottom.
+	 * @param {boolean} [displayZero=false] True if the number zero should be displayed,
+	 *  false if the number zero should result in the badge being hidden
+	 * @return {jQuery}
+	 * @chainable
+	 */
+	$.fn.badge = function ( text, position, displayZero ) {
+		var $badge = this.find( '.badge' ),
+			badgeStyleClass,
+			isImportant = true,
+			displayBadge = true;
+
+		// Set the position of the badge
+		if ( position === 'inline' ||
+			position === 'top' ||
+			position === 'bottom'
+		) {
+			badgeStyleClass = 'badge-' + position;
+		} else {
+			badgeStyleClass = 'badge-top';
+		}
+
+		// If we're displaying zero, ensure style to be non-important (grey instead of red)
+		if ( text === 0 ) {
+			isImportant = false;
+			if ( !displayZero ) {
+				displayBadge = false;
+			}
+		// If text is falsey (besides 0), hide the badge
+		} else if ( !text ) {
+			displayBadge = false;
+		}
+
+		if ( displayBadge ) {
+console.debug(1);
+			// If a badge already exists, reuse it
+			if ( $badge.length ) {
+				$badge
+					.toggleClass( 'badge-important', isImportant )
+					.find( '.badge-content' ).text( text );
+			} else {
+				// Otherwise, create a new badge with the specified text and style
+				$badge = $( '<div class="badge"></div>' )
+					.addClass( badgeStyleClass )
+					.toggleClass( 'badge-important', isImportant )
+					.append(
+						$( '<span class="badge-content"></span>' ).text( text )
+					)
+					.appendTo( this );
+			}
+		} else {
+			$badge.remove();
+		}
+		return this;
+	};
+
+	/**
+	 * @class jQuery
+	 * @mixins jQuery.plugin.badge
+	 */
+}( jQuery ) );
